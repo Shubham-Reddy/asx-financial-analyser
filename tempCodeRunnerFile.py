bc10cb0eb0ef6ab889a83d66744ed72976da274b
Shubham <shubhamreddy2001@gmail.com>
2026-06-21 01:14:53 +1000

    Remove PDFs and temp files from repository

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-import pdfplumber
-
-def extract_text(pdf_file):
-    text = ""
-    with pdfplumber.open(pdf_file) as pdf:
-        for page in pdf.pages:
-            text += page.extract_text() or ""
-    return text
